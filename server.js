ed5cfe0f894bec8aa09b1742406200521163ed9c
Aisha <aisha@imperialdigital.co.nz>
2017-06-27 11:34:58 +1200

    make it better

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var url = "mongodb://localhost:27017/voomdb";
+
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
