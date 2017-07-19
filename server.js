e1a5594892887608d808d695ad817155b22ed451
Aisha <aisha@imperialdigital.co.nz>
2017-07-19 16:15:50 +1200

    f

--- server.js
+++ server.js
@@ -7,20 +7,13 @@ const app            = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
 
 MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-
-  app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-
-  // //load up index file
-  // app.get('/', function(req, res){
-  //     res.render('index.html');
-  // });
-
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });
