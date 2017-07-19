1cdd2eb84bf11488a73a6d75197a10dae6cf7484
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 10:14:23 +1200

    s

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app            = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static(__dirname + "/Public"));
 
 var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
 
