8392379fb105e3a6bb77ed752b11f7b42f52ca91
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 11:22:19 +1200

    n

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,6 +1,26 @@
 //user_routes.js
+
+// if our user.js file is at app/models/user.js
+var User = require('./app/models/user');
+
+
 module.exports = function(app, db) {
   app.post('/user', (req, res) => {
+
+    // create a new user called chris
+    var chris = new User({
+      name: 'Chris',
+      username: 'sevilayha',
+      password: 'password'
+    });
+
+    // call the built-in save method to save to the database
+    chris.save(function(err) {
+      if (err) throw err;
+
+      console.log('User saved successfully!');
+    });
+    
     const user = { email: req.body.email, password: req.body.password };
     db.collection('user').insert(user, (err, result) => {
       if (err) {
