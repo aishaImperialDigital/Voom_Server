32b813a560d213abfb6a5a0385fd7087c5a22eb1
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 11:20:44 +1200

    v

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,9 @@
+// grab the things we need
 var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
+var Schema = mongoose.Schema;
 
+// create a schema
+var userSchema = new Schema({
   username: {
     type: String,
     unique: true,
@@ -56,6 +59,18 @@ var UserSchema = new mongoose.Schema({
     required: true,
   }
 
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
 });
-var User = mongoose.model('User', UserSchema);
+
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+// make this available to our users in our Node applications
 module.exports = User;
