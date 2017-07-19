82bc46ffd9346da78bcb36cd14ed7f7d2d2fb4f3
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 11:07:14 +1200

    g

--- config/db.js
+++ config/db.js
@@ -2,9 +2,3 @@ module.exports = {
     // the database url to connect
     url : 'mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb'
 }
-
-var login_db = mongoose.model('mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb', db_schema);
-       return function (req, res, next) {
-               req.app = login_db;
-               next();
-           }; 
--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+var mongoose = require('mongoose');
 const app            = express();
-
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/Public"));
 
 var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
-
+mongoose.connect(url);
 MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
