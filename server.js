9879bb6e2a0c4dd88ce57db6d77bd771f05f4138
Aisha <aisha@imperialdigital.co.nz>
2017-07-19 16:02:48 +1200

    html added

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
 MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
+
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+
+  //load up index file
+  app.get('/', function(req, res){
+      res.render('index.html');
+  });
+
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });
