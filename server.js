d4c1b13b90219ab8db1cea92fea57d29a3d5e942
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 10:02:52 +1200

    x

--- server.js
+++ server.js
@@ -1,25 +1,55 @@
-// server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = express();
+// // server.js
+// const express        = require('express');
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
+// const app            = express();
 
-const port = 8000;
+// const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+// app.get('/', function(req, res){
+//   res.render('form');// if jade
+//   // You should use one of line depending on type of frontend you are with
+//   res.sendFile(__dirname + '/form.html'); //if html file is root directory
+//  res.sendFile("index.html"); //if html file is within public directory
+// });
+// var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
+
+// MongoClient.connect(url, (err, database) => {
+//   if (err) return console.log(err)
+//   require('./app/routes')(app, database);
+//   app.listen(port, () => {
+//     console.log('We are live on ' + port);
+//   });
+// })
+const port = 3000;
+
+var express = require('express'),
+    app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+   extended: false;
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
   res.render('form');// if jade
   // You should use one of line depending on type of frontend you are with
   res.sendFile(__dirname + '/form.html'); //if html file is root directory
  res.sendFile("index.html"); //if html file is within public directory
 });
-var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
 
-MongoClient.connect(url, (err, database) => {
-  if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
-  });
-})
+app.post('/',function(req,res){
+   var username = req.body.username;
+   var htmlData = 'Hello:' + username;
+   res.send(htmlData);
+   console.log(htmlData);
+});
+
+app.listen(port);
