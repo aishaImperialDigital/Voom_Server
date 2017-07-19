0659f5aeb653bb17f68282c177744ce1d5f62853
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 11:38:38 +1200

    gf

--- server.js
+++ server.js
@@ -18,21 +18,6 @@ var User = require('./app/models/user');
    password: 'password'
  });
 
- // on every save, add the date
- userSchema.pre('save', function(next) {
-   // get the current date
-   var currentDate = new Date();
-
-   // change the updated_at field to current date
-   this.updated_at = currentDate;
-
-   // if created_at doesn't exist, add to that field
-   if (!this.created_at)
-     this.created_at = currentDate;
-
-   next();
- });
-
  // call the built-in save method to save to the database
  chris.save(function(err) {
    if (err) throw err;
