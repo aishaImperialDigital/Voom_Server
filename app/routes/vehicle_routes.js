7acc233def12b6c2c48c3c82f5636e571feda264
Aisha <aisha@imperialdigital.co.nz>
2017-07-19 15:53:04 +1200

    changes

---
+++ app/routes/vehicle_routes.js
@@ -0,0 +1,13 @@
+// user_routes.js
+module.exports = function(app, db) {
+  app.post('/vehicle', (req, res) => {
+    const user = { email: req.body.email, password: req.body.password };
+    db.collection('user').insert(user, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
