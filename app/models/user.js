e9344f2991b496440ce6b194b8ff2a4efbc8fcf1
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 13:22:28 +1200

    d

--- app/models/user.js
+++ app/models/user.js
@@ -27,3 +27,15 @@ var UserSchema = new Schema({
 });
 
 module.exports = mongoose.model('Users', UserSchema);
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
