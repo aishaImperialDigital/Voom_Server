576bfcc8309304d31ec14bee1fa8cd6a7b3d2eb7
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 15:02:01 +1200

    f

--- server.js
+++ server.js
@@ -68,32 +68,5 @@ app.use(session({
   })
 }));
 
-function requiresLogin(req, res, next) {
-  if (req.session && req.session.userId) {
-    return next();
-  } else {
-    var err = new Error('You must be logged in to view this page.');
-    err.status = 401;
-    return next(err);
-  }
-}
-router.get('/profile', mid.requiresLogin, function(req, res, next) {
-  //...
-});
-// GET /logout
-router.get('/logout', function(req, res, next) {
-  if (req.session) {
-    // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
-});
-
-
 
 console.log('user RESTful API server started on: ' + port);
