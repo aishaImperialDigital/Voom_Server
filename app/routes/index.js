a8057039fc0fd877bc6fc7fe9640c1a7347676f5
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 12:29:52 +1200

    c

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 // routes/index.js
-const userRoutes = require('./user_routes');
-const vehicleRoutes = require('./vehicle_routes');
-module.exports = function(app, db) {
-  userRoutes(app, db);
-  // Other route groups could go here, in the future
-  vehicleRoutes(app, db);
-};
+// const userRoutes = require('./user_routes');
+// const vehicleRoutes = require('./vehicle_routes');
+// module.exports = function(app, db) {
+//   userRoutes(app, db);
+//   // Other route groups could go here, in the future
+//   vehicleRoutes(app, db);
+// };
--- server.js
+++ server.js
@@ -1,32 +1,56 @@
 // server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-var mongoose = require('mongoose');
-const app            = express();
-const port = 8000;
+// const express        = require('express');
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
+// var mongoose = require('mongoose');
+// const app            = express();
+// const port = 8000;
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + "/Public"));
+// var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
+// mongoose.connect(url);
+// var User = require('./app/models/user.js');
+//  // create a new user called chris
+//  var chris = new User({
+//    name: 'Chrccisff',
+//    username: 'sevilayha',
+//    password: 'password'
+//  });
+//
+//  // call the built-in save method to save to the database
+//  chris.save(function(err) {
+//    if (err) throw err;
+//
+//    console.log('User saved successfully!');
+//  });
+// MongoClient.connect(url, (err, database) => {
+//   if (err) return console.log(err)
+//   require('./app/routes')(app, database);
+//   app.listen(port, () => {
+//     console.log('We are live on ' + port);
+//   });
+// })
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  User = require('./app/models/user'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb');
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/Public"));
-var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
-mongoose.connect(url);
-var User = require('./app/models/user.js');
- // create a new user called chris
- var chris = new User({
-   name: 'Chrccisff',
-   username: 'sevilayha',
-   password: 'password'
- });
+app.use(bodyParser.json());
 
- // call the built-in save method to save to the database
- chris.save(function(err) {
-   if (err) throw err;
 
-   console.log('User saved successfully!');
- });
-MongoClient.connect(url, (err, database) => {
-  if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
-  });
-})
+var routes = require('./api/routes/user_routes');
+routes(app);
+
+
+app.listen(port);
+
+
+console.log('user RESTful API server started on: ' + port);
