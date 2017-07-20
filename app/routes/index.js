5ecd6d1a34d5c28899d853879cd732d9b2a84948
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 15:13:18 +1200

    d

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 //routes/index.js
 const userRoutes = require('./user_routes');
-const userLoginRoutes = require('./routes');
+const userLoginRoutes = require('./vehicle_routes');
 module.exports = function(app, db) {
   userRoutes(app, db);
   // Other route groups could go here, in the future
