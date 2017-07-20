f06d96c0f61e9a6b04b4b3d41a7e9c8b4d032b9a
Aisha <aisha@imperialdigital.co.nz>
2017-07-20 16:42:24 +1200

    f

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer");
 var User = require('../models/user');
+
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "imperialdigital02@gmail.com",
+        pass: "Imperial01!!"
+    }
+});
+var rand,mailOptions,host,link;
+/*------------------SMTP Over-----------------------------*/
 
 
 // GET route for reading data
@@ -36,6 +51,23 @@ router.post('/', function (req, res, next) {
         return next(error);
       } else {
         req.session.userId = user._id;
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
         return res.redirect('/profile');
       }
     });
@@ -88,6 +120,50 @@ router.get('/logout', function (req, res, next) {
       }
     });
   }
+});
+
+router.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+
+router.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("<h1>Bad Request</h1>");
+    }
+}
+else
+{
+    res.end("<h1>Request is from unknown source");
+}
 });
 
 module.exports = router;
