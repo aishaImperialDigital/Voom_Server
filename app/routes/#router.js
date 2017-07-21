aea5ff8b71181861a45732dd5167700ac58f8b9b
Aisha <aisha@imperialdigital.co.nz>
2017-07-21 13:51:58 +1200

    c

--- app/routes/#router.js
+++ app/routes/#router.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-
-
+var User = require('./models/user');
 // GET route for reading data
 router.get('/', function (req, res, next) {
   return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
--- server.js
+++ server.js
@@ -1,76 +1,3 @@
-// server.js
-// const express        = require('express');
-// const MongoClient    = require('mongodb').MongoClient;
-// const bodyParser     = require('body-parser');
-// var mongoose = require('mongoose');
-// const app            = express();
-// const port = 8000;
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + "/Public"));
-// var url = "mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb";
-// mongoose.connect(url);
-// var User = require('./app/models/user.js');
-//  // create a new user called chris
-//  var chris = new User({
-//    name: 'Chrccisff',
-//    username: 'sevilayha',
-//    password: 'password'
-//  });
-//
-//  // call the built-in save method to save to the database
-//  chris.save(function(err) {
-//    if (err) throw err;
-//
-//    console.log('User saved successfully!');
-//  });
-// MongoClient.connect(url, (err, database) => {
-//   if (err) return console.log(err)
-//   require('./app/routes')(app, database);
-//   app.listen(port, () => {
-//     console.log('We are live on ' + port);
-//   });
-// })
-
-///////////// end
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
-// var express = require('express'),
-//   app = express(),
-//   port = process.env.PORT || 8000,
-//   mongoose = require('mongoose'),
-//   User = require('./app/models/user'),
-//   bodyParser = require('body-parser');
-//
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb');
-//
-//
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-//
-//
-// var routes = require('./app/routes/user_routes');
-// routes(app);
-//
-// app.listen(port);
-//
-// app.use(function(req, res) {
-//   res.status(404).send({url: req.originalUrl + ' not found'})
-// });
-// //use sessions for tracking logins
-// //use sessions for tracking logins
-// app.use(session({
-//   secret: 'work hard',
-//   resave: true,
-//   saveUninitialized: false,
-//   store: new MongoStore({
-//     mongooseConnection: mongoose.connection,
-//     touchAfter: 24 * 3600 // time period in seconds
-//   })
-// }));
-//
-//
-// console.log('user RESTful API server started on: ' + port);
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -107,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/templateLogReg'));
 
 // include routes
-var routes = require('./app/routes/router');
+var routes = require('./routes/router');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -126,6 +53,6 @@ app.use(function (err, req, res, next) {
 
 
 // listen on port 3000
-app.listen(8002, function () {
-  console.log('Express app listening on port 8002');
+app.listen(8000, function () {
+  console.log('Express app listening on port 8000');
 });
