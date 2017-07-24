b76c798dbdadaac483dc4df2fdf8cd2c9212e61a
Aisha <aisha@imperialdigital.co.nz>
2017-07-24 14:11:45 +1200

    d

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-
+$http.defaults.headers.post.Authorization = "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
 //connect to MongoDB
 mongoose.connect('mongodb://voomdb:voomdb@ds163232.mlab.com:63232/voomdb');
 var db = mongoose.connection;
